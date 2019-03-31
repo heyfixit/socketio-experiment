5fd62b9ccd0b7e65447dc47ec31d930d6bf70210
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-30 22:01:11 -0400

    remove commented code, add title

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const port = process.env.PORT || 4000;
 
-// const actionsRouter = require('./actions/router');
-// const projectsRouter = require('./projects/router');
-// server.use('/api/projects', projectsRouter);
-// server.use('/api/actions', actionsRouter);
-
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
@@ -19,7 +14,6 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 io.on('connection', socket => {
   console.log('new socketio connection');
   socket.on('disconnect', () => console.log('client disconnected'));
-  // socket.on('drawing', data => {});
   socket.on('draw-line', data => {
     socket.broadcast.emit('draw-line', data);
   });
@@ -32,7 +26,4 @@ app.get('*', (req, res) => {
 
 server.listen(port, () => {
   console.log(`\n*** API running on  port ${port} ***\n`);
-  // setInterval(() => {
-  //   io.sockets.emit('test', 'test');
-  // }, 1000);
 });
